01c27f795f90f22df250be1f58f9ed34902229d1
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-04-03 12:44:20 +0200

    styling

--- src/components/AlbumPage/index.jsx
+++ src/components/AlbumPage/index.jsx
@@ -5,12 +5,14 @@ import styles from './AlbumPage.module.css'
 const AlbumPage = ({album, updateFunction}) => {
     return (
         <div className={styles.albumPage}>
-            < img src={album.cover} alt='Album cover'/>
-            <h3 className={styles.albumTitle}>{album.albumTitle}</h3>
-            <h4 className={styles.artistName}>{album.artist}</h4> 
-            {album.songs.map((song, index) => 
-            <p className={styles.songTitle} key={index}>{song} </p> )}
-            < BackButton choice='library' updatePage={updateFunction} />
+            <div className={styles.albumArea}>
+                <img src={album.cover} alt='Album cover'/>
+                <h3 className={styles.albumTitle}>{album.albumTitle}</h3>
+                <h4 className={styles.artistName}>{album.artist}</h4> 
+                {album.songs.map((song, index) => 
+                <p className={styles.songTitle} key={index}>{song} </p> )}
+            </div>
+            < BackButton className={styles.backToHomeBtn} choice='library' updatePage={updateFunction} />
         </div>
     )
 }
