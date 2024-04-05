791fd38f124c87878e8a1cfbdf729c8d5a4a4945
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-04-05 16:26:24 +0200

    style about page

--- src/components/AboutPage/index.jsx
+++ src/components/AboutPage/index.jsx
@@ -4,7 +4,7 @@ import styles from './AboutPage.module.css'
 const AboutPage = ({updateFunction}) => {
     
     return(
-        <div className={styles.AboutPage}>
+        <div className={styles.aboutPage}>
             <h1>About</h1>
             <p>This is a site with some of my most listened albums the past time. It was created with the purpose of practicing the fundamentals of react and specifically passing state between components.</p>
             <BackButton choice='library' updatePage={updateFunction} />
